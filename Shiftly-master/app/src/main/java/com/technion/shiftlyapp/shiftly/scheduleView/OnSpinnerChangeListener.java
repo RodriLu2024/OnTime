89ae85b6c046c6fe482eb7c793a27fc25e637bb7
RodriLu2024 <lucia.rodriguez@alumnos.udemm.edu.ar>
2024-08-12 20:23:11 +0200

    Cambio carpetas

--- Shiftly-master/app/src/main/java/com/technion/shiftlyapp/shiftly/scheduleView/OnSpinnerChangeListener.java
+++
@@ -1,6 +0,0 @@
-package com.technion.shiftlyapp.shiftly.scheduleView;
-
-// Interface for managing the spinners in the ScheduleEditActivity
-public interface OnSpinnerChangeListener {
-    void onSpinnerChange(int index, String employee_name);
-}
